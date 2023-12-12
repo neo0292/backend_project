0eccdd4a59e79c285441d5e3f7272af38315950e
neo0292 <110742271+neo0292@users.noreply.github.com>
2023-12-12 22:52:51 +0530

    cloudinary util added

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,8 +1,10 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
-    userName: 
+    username: 
     { 
      type: String,
      required: true,
@@ -47,5 +49,51 @@ const userSchema = new Schema(
       ref: 'Video'
     }
   }, {timestamps: true});
+
+  //use pre hook to encrypt password just before saving in db
+userSchema.pre('save', async function (next) {
+  if(!this.isModified('password')) return next();
+  
+  this.password = bcrypt.hash(this.password,10)
+  next();
+})
+
+// custom method to verify password
+userSchema.methods.isPasswordCorrect = async function (password){
+  return await bcrypt.compare(password,this.password)
+}
+
+// custom method to generate AccessToken
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign(
+    {//data
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName
+    },
+    //secret
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      //expiry
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+
+//custom method to generate refreshToken
+userSchema.methods.generatRefreshToken = function (){
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
+
+
 
 export const User = mongoose.model('User', userSchema);
